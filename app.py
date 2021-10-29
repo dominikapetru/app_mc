86a566a006d0e851b3b7c222e9e99aa49b258dc4
Dominique <Dominika.Petru@gmail.com>
2021-10-29 13:15:12 +0200

    ok

--- app.py
+++ app.py
@@ -5,18 +5,24 @@ import numpy as np
 st.header(XAPAPP)
 st.write('Aplicació intel·ligent per canviar els preus dels articles.')
 st.subheader('Entrada d\'usuari')
-uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['xlsx'])
-
+uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['cvs'])
 
 if uploaded_file is not None:
     st.write('S\'ha pujat un fitxer CSV.')
-    df = pd.read_csv(uploaded_file)
-    df_mod = df.groupby(['Descripción']).first().reset_index()
-    d1 = df_mod[['Descripción', 'Nombre', 'Coste']]
-    st.write(d1)
+    df1 = pd.read_csv(uploaded_file)
+    df_mod = df1.groupby(['Descripción']).first().reset_index()
+    df = df_mod[['Descripción', 'Nombre', 'Coste']]
+    st.write(df)
 else:
     st.write('S\'està esperant el fitxer CSV per pujar. Actualment s\'utilitzen paràmetres d\'entrada d\'exemple.')
-    df_example = pd.read_csv('https://raw.githubusercontent.com/dominikapetru/app_mc/main/articles.csv')
-    df_mod = df_example.groupby(['Descripción']).first().reset_index()
-    d2 = df_mod[['Descripción', 'Nombre', 'Coste']]
-    st.write(d2)
+    df2 = pd.read_csv('https://raw.githubusercontent.com/dominikapetru/app_mc/main/articles.csv')
+    df_mod = df2.groupby(['Descripción']).first().reset_index()
+    df = df_mod[['Descripción', 'Nombre', 'Coste']]
+    st.write(df)
+
+st.subheader('Canvi de preus per familia d\'articles')
+
+price0_des = df['Descripción'].values[0]
+price0 = st.number_input(label: price0_des)
+
+
