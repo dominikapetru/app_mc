02667a48967c5115ef0d6b0e9617e9e5f18f8e75
Dominique <Dominika.Petru@gmail.com>
2021-10-29 09:52:40 +0200

    ok

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import pandas as pd
-import datetime
+import numpy as np
 
+# App config.
 DEBUG = True
 app = Flask(__name__,template_folder='static')
 app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
