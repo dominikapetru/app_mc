32f4101d9633909f329d5573730f01e90086b453
Dominique <Dominika.Petru@gmail.com>
2021-10-29 09:58:49 +0200

    ok

--- app.py
+++ app.py
@@ -4,7 +4,3 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
-
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
