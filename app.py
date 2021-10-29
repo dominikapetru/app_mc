544132e5324275f53d4c4ed1e2fee6d0b38d4f65
Dominique <Dominika.Petru@gmail.com>
2021-10-29 10:42:48 +0200

    ok

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("""Aplicació intel·ligent per canviar preus dels articles""")
+st.write("""
+# XAPAPP
+Aplicació intel·ligent per canviar els preus dels articles.
+""")
+
+st.subheader('Entrada d\'usuari')
+
+
