602eff395a7fb6860abdbfc43a8d073166b03859
Dominique <Dominika.Petru@gmail.com>
2021-10-29 12:52:06 +0200

    ok

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ Aplicació intel·ligent per canviar els preus dels articles.
 st.subheader('Entrada d\'usuari')
 uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['xlsx'])
 
+
 if uploaded_file is not None:
     st.write('S\'ha pujat un fitxer CSV.')
     df = pd.read_csv(uploaded_file)
-    d1 = df.groupby(['Descripción']).first().reset_index()
-    d1[['Descripción', 'Nombre', 'Coste']]
+    df_mod = df.groupby(['Descripción']).first().reset_index()
+    d1 = df_mod[['Descripción', 'Nombre', 'Coste']]
     st.write(d1)
 else:
     st.write('S\'està esperant el fitxer CSV per pujar. Actualment s\'utilitzen paràmetres d\'entrada d\'exemple.')
     df_example = pd.read_csv('https://raw.githubusercontent.com/dominikapetru/app_mc/main/articles.csv')
-    d2 = df_example.groupby(['Descripción']).first().reset_index()
-    d2[['Descripción', 'Nombre', 'Coste']]
+    df_mod = df_example.groupby(['Descripción']).first().reset_index()
+    d2 = df_mod[['Descripción', 'Nombre', 'Coste']]
     st.write(d2)
