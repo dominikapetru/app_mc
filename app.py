0fc4dc88eb66526fe47b524f05fb51cb1ca9f53a
Dominique <Dominika.Petru@gmail.com>
2021-10-29 12:10:27 +0200

    ok

--- app.py
+++ app.py
@@ -8,7 +8,15 @@ Aplicació intel·ligent per canviar els preus dels articles.
 """)
 
 st.subheader('Entrada d\'usuari')
-uploaded_file = st.file_uploader("Carrega el fitxer CSV", type=["csv"])
+uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['xlsx'])
 
-
-
+if uploaded_file is not None:
+    st.write('S\'ha pujat un fitxer CSV.')
+    df = pd.read_csv(uploaded_file)
+    df = df[:5]
+    st.write(df)
+else:
+    st.write('S\'està esperant el fitxer CSV per pujar. Actualment s\'utilitzen paràmetres d\'entrada d\'exemple.')
+    df_example = pd.read_csv('https://raw.githubusercontent.com/dominikapetru/app_mc/main/articles.csv')
+    df_example = df_example[:5]
+    st.write(df_example)
