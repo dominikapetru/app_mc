2813daacbf8601199d0e9bafb2201ac47bf5468f
Dominique <Dominika.Petru@gmail.com>
2021-10-29 13:23:15 +0200

    ok

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header(XAPAPP)
+st.write("""XAPAPP
+""")
 st.write('Aplicació intel·ligent per canviar els preus dels articles.')
 st.subheader('Entrada d\'usuari')
 uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['cvs'])
