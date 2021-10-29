b38972589f869a074ee985e9f63cbccb06d1ad53
Dominique <Dominika.Petru@gmail.com>
2021-10-29 17:43:58 +0200

    ok

--- app.py
+++ app.py
@@ -22,28 +22,44 @@ else:
 
 st.subheader('Canvi de preus per familia d\'articles')
 
+# names of the groups of articles
 var = df['Descripción'].unique()
 desc = var.tolist()
 
+# select one group
 option1 = st.selectbox(
     'Quina familia de preus vols canviar?',
     desc)
 st.write('Has seleccionat:', option1)
 
+# select one group
 option2 = st.number_input(
     'Quin és el nou preu?')
 st.write('Has introduït:', option2)
 
+# change the group
+if option1 and option2 is not None:
+    df_new = df[df['Descripción'] == 'option1']
+    df_new = df_new.assign(Coste=option2)
+
+else:
+    df_new = df
+
+st.write('Actualització final')
+df3 = df_new.groupby(['Descripción']).first().reset_index()
+df_final = df3[['Descripción', 'Nombre', 'Coste']]
+st.write(df_final)
+
 
 @st.cache
-def convert_df(df):
-    return df.to_csv().encode('utf-8')
+def convert_df(df_final):
+    return df_final.to_csv().encode('utf-8')
 
 
-csv = convert_df(df)
+csv = convert_df(df_final)
 st.download_button(
     label="Baixa les dades com a CSV",
     data=csv,
-    file_name='large_df.csv',
+    file_name='articles.csv',
     mime='text/csv',
     )
