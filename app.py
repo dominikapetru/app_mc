1b06f68887b58590d3d0769eb26e86f171af4f2c
Dominique <Dominika.Petru@gmail.com>
2021-10-29 12:43:48 +0200

    ok

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("""
+st.header("""
 # XAPAPP
 Aplicació intel·ligent per canviar els preus dels articles.
 """)
@@ -13,10 +13,12 @@ uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['xlsx'])
 if uploaded_file is not None:
     st.write('S\'ha pujat un fitxer CSV.')
     df = pd.read_csv(uploaded_file)
-    df = df[:5]
-    st.write(df)
+    d1 = df.groupby(['Descripción']).first().reset_index()
+    d1[['Descripción', 'Nombre', 'Coste']]
+    st.write(d1)
 else:
     st.write('S\'està esperant el fitxer CSV per pujar. Actualment s\'utilitzen paràmetres d\'entrada d\'exemple.')
     df_example = pd.read_csv('https://raw.githubusercontent.com/dominikapetru/app_mc/main/articles.csv')
-    df_example = df_example[:5]
-    st.write(df_example)
+    d2 = df_example.groupby(['Descripción']).first().reset_index()
+    d2[['Descripción', 'Nombre', 'Coste']]
+    st.write(d2)
