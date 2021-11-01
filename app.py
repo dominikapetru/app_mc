43c1b960ccbbddc0040dcc6fe84a947654c92405
Dominique <Dominika.Petru@gmail.com>
2021-11-01 16:23:23 +0100

    ok

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
+from io import BytesIO
 
 st.title('XAPAPP')
 st.write('Aplicació intel·ligent per canviar els preus dels articles.')
 st.subheader('Entrada d\'usuari')
-uploaded_file = st.file_uploader('Carrega el fitxer CSV', type=['csv'])
+uploaded_file = st.file_uploader('Carrega el fitxer XLSX', type=['xlsx'])
 
 if uploaded_file is not None:
-    st.write('S\'ha pujat un fitxer CSV.')
-    df1 = pd.read_csv(uploaded_file)
+    st.write('S\'ha pujat un fitxer XLSX.')
+    df1 = pd.read_excel(uploaded_file)
     df_mod = df1.groupby(['Descripción']).first().reset_index()
     df = df_mod[['Descripción', 'Nombre', 'Coste']]
     st.write(df)
@@ -44,16 +46,22 @@ st.subheader('Actualització final')
 df_final = df.loc[df.Descripción == option1, "Coste"] = option2
 st.write(df_final)
 
-# colect data and export file
-@st.cache
-def convert_df(df_final):
-    return df_final.to_csv().encode('utf-8')
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
 
 
-csv = convert_df(df_final)
-st.download_button(
-    label="Baixa les dades com a CSV",
-    data=csv,
-    file_name='articles.csv',
-    mime='text/csv',
-    )
+def get_table_download_link(df):
+    val = to_excel(df)
+    b64 = base64.b64encode(val)
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>'
+
+
+# collect data and export file
+df = df_final
+st.markdown(get_table_download_link(df), unsafe_allow_html=True)
