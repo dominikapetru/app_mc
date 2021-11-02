44e40807fb872255d7585b4aba8d1f04d8a01129
Dominique <Dominika.Petru@gmail.com>
2021-11-02 14:43:17 +0100

    ok

--- app.py
+++ app.py
@@ -39,22 +39,16 @@ var = df['Descripción'].unique()
 desc = var.tolist()
 
 
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-
-
 # select one group
 option1 = st.selectbox('Quina familia de preus vols canviar?', desc)
 st.write('Has seleccionat:', option1)
-if option1:
-    option1 = str(option1)
+option1 = str(option1)
 
 # select new price
 option2 = st.number_input('Quin és el nou preu?')
 st.write('Has introduït:', option2)
 option2 = float(option2)
-if option2:
-    option1 = str(option2)
+option1 = str(option2)
 
 
 # button
